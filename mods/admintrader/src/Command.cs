0f385b8d16b5cf530bb2f1e09c2eb3393c4e7a1e
Monica Björk <m.bjork95@gmail.com>
2023-01-26 09:27:41 +0100

    Playing around a bit with commands

--- mods/admintrader/src/Command.cs
+++ mods/admintrader/src/Command.cs
@@ -10,16 +10,18 @@ using Vintagestory.API.Server;
 namespace admintrader.src {
     public class Command : ModSystem {
 
-        // Load mod on server start
+        // Get serverside
         public override bool ShouldLoad(EnumAppSide side) {
             return side == EnumAppSide.Server;
         }
         
         // Register command
         public override void StartServerSide(ICoreServerAPI api) {
+            // Start server
             base.StartServerSide(api);
 
             AssetLocation sound = new AssetLocation("here", "sounds/partyhorn"); // Create sound location
+            // register command
             api.RegisterCommand("here", "spawns particles around the player", "",
                 (IServerPlayer player, int groupId, CmdArgs args) => {
                     EntityPlayer byEntity = player.Entity;
@@ -31,6 +33,23 @@ namespace admintrader.src {
                         Vec3d realPos = pos.AddCopy(-0.1 + (rand.NextDouble() * 0.2), 0, -0.1 + (rand.NextDouble() * 0.2));
                         Vec3f velocity = new Vec3f(-0.2F + ((float)rand.NextDouble() * 0.4F), 0.4F + ((float)rand.NextDouble() * 2F), -0.2F + ((float)rand.NextDouble() * 0.4F));
                         byEntity.World.SpawnParticles(1, ColorUtil.ColorFromRgba(255, rand.Next(0, 255), rand.Next(0, 255), rand.Next(0, 255)),
+                        realPos, realPos,
+                        velocity, velocity, ((float)rand.NextDouble() * 1) + 1, 0.01F,
+                        1, EnumParticleModel.Cube);
+                    }
+                }, Privilege.chat);
+
+            api.RegisterCommand("heree", "spawns particles around the player", "",
+                (IServerPlayer player, int groupId, CmdArgs args) => {
+                    EntityPlayer byEntity = player.Entity;
+                    byEntity.World.PlaySoundAt(sound, byEntity); // Play sound
+
+                                Vec3d pos = byEntity.Pos.XYZ.Add(0, byEntity.LocalEyePos.Y, 0); // Setting up position to spawn particles
+                                Random rand = new Random();
+                    for(int i = 0; i < 100; i++) { // Spawn 100 particles 
+                                    Vec3d realPos = pos.AddCopy(-0.1 + (rand.NextDouble() * 0.2), 0, -0.1 + (rand.NextDouble() * 0.2));
+                        Vec3f velocity = new Vec3f(-0.2F + ((float)rand.NextDouble() * 0.4F), 0.4F + ((float)rand.NextDouble() * 2F), -0.2F + ((float)rand.NextDouble() * 0.4F));
+                        byEntity.World.SpawnParticles(1, ColorUtil.ColorFromRgba(155, rand.Next(0, 155), rand.Next(0, 155), rand.Next(0, 155)),
                         realPos, realPos,
                         velocity, velocity, ((float)rand.NextDouble() * 1) + 1, 0.01F,
                         1, EnumParticleModel.Cube);
