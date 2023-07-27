b2b91ed77ab0d488c3fbcc564880ca70fd66c849
Monica Björk <m.bjork95@gmail.com>
2023-07-27 15:54:51 +0200

    added commands, not yet implemented

--- mods/adminbook/src/AdminBook.cs
+++ mods/adminbook/src/AdminBook.cs
@@ -1,17 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
-using Vintagestory.API.Client;
 using Vintagestory.API.Common;
-using Vintagestory.API.Config;
-using Vintagestory.API.Datastructures;
-using Vintagestory.API.MathTools;
+using Vintagestory.API.Common.CommandAbbr;
 using Vintagestory.API.Server;
-using Vintagestory.API.Util;
 using Vintagestory.GameContent;
+
 
 namespace adminbook.src 
 {
@@ -28,7 +19,6 @@ namespace adminbook.src
     {
 		// Some code here maybe?
         // TODO: Create a player friendly command to get an adminbook
-        // TODO: Make sure the book can't be used as fuel! 
         // TODO: figure out how to populate the '/help adminbook' command
 	}
 
@@ -42,12 +32,52 @@ namespace adminbook.src
         public override void StartServerSide(ICoreServerAPI api) 
         {
             base.StartServerSide(api);
-            string command = "adminbook";
-            string description = "edit the adminbook";
+            //string command = "adminbook";
+            //string description = "edit the adminbook";
 
-            api.RegisterCommand(command, description, "[edit|give]", adminBookCmd, Privilege.commandplayer);
+            //api.RegisterCommand(command, description, "[edit|give]", adminBookCmd, Privilege.commandplayer);
+
+            var parsers = api.ChatCommands.Parsers;
+            api.ChatCommands.Create("adminbook")
+            .WithAlias("ab")
+            .RequiresPrivilege(Privilege.commandplayer)
+            .BeginSubCommand("edit")
+                .WithDescription("Edit the adminbook")
+                .HandleWith(adminBookEdit)
+            .EndSubCommand()
+            .BeginSubCommand("get")
+                .RequiresPrivilege(Privilege.chat)
+                .WithDescription("Give the adminbook to oneself")
+                .HandleWith(adminBookGet)
+            .EndSubCommand()
+            .BeginSubCommand("give")
+                .WithDescription("Give the adminbook to a player")
+                .WithArgs(parsers.Word("player name"))
+                .HandleWith(adminBookGive)
+            .EndSubCommand();
         }
 
+        private TextCommandResult adminBookEdit(TextCommandCallingArgs args)
+        {
+            return TextCommandResult.Success("Command to edit the admin book");
+        }
+
+        private TextCommandResult adminBookGive(TextCommandCallingArgs args)
+        {
+            // if args matches player, give book
+            
+            return TextCommandResult.Success("Command to give an admin book");
+        }
+
+        private TextCommandResult adminBookGet(TextCommandCallingArgs args)
+        {
+            // if args matches player, give book
+
+            return TextCommandResult.Success("Command to give an admin book to player");
+        }
+
+
+        /*
         private void adminBookCmd(IServerPlayer player, int groupId, CmdArgs args) 
         {
             //ItemStack adminbookItem = new ItemStack(3042, ItemBooks, 1, TreeAttribute stackAttributes, IWorldAccessor resolver);
@@ -83,5 +113,7 @@ namespace adminbook.src
                 // TODO: Write some info about the mod / commands
             }
         }
+        */
+
     }
 }
